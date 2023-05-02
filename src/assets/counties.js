b95a66f60e9e88b0a934399f2460d7831572dbca
Kris Hvattum <klhvattum@live.com>
2023-05-02 14:58:05 -0700

    Add rest of react project

---
+++ src/assets/counties.js
@@ -0,0 +1,198 @@
+import Adams from '../assets/WA Counties/Adams.gif';
+import Asotin from '../assets/WA Counties/Asotin.gif';
+import Benton from '../assets/WA Counties/Benton.gif';
+import Chelan from '../assets/WA Counties/Chelan.gif';
+import Clallam from '../assets/WA Counties/Chelan.gif';
+import Clark from '../assets/WA Counties/Chelan.gif';
+import Columbia from '../assets/WA Counties/Columbia.gif';
+import Cowlitz from '../assets/WA Counties/Cowlitz.gif';
+import Douglas from '../assets/WA Counties/Douglas.gif';
+import Ferry from '../assets/WA Counties/Ferry.gif';
+import Franklin from '../assets/WA Counties/Franklin.gif';
+import Garfield from '../assets/WA Counties/Garfield.gif';
+import Grant from '../assets/WA Counties/Grant.gif';
+import GraysHarbor from '../assets/WA Counties/Grays Harbor.gif';
+import Island from '../assets/WA Counties/Island.gif';
+import Jefferson from '../assets/WA Counties/Jefferson.gif';
+import King from '../assets/WA Counties/King.gif';
+import Kitsap from '../assets/WA Counties/Kitsap.gif';
+import Kittitas from '../assets/WA Counties/Kittitas.gif';
+import Klickitat from '../assets/WA Counties/Klickitat.gif';
+import Lewis from '../assets/WA Counties/Lewis.gif';
+import Lincoln from '../assets/WA Counties/Lincoln.gif';
+import Mason from '../assets/WA Counties/Mason.gif';
+import Okanogan from '../assets/WA Counties/Okanogan.gif';
+import Pacific from '../assets/WA Counties/Pacific.gif';
+import PendOreille from '../assets/WA Counties/Pend Oreille.gif';
+import Pierce from '../assets/WA Counties/Pierce.gif';
+import SanJuan from '../assets/WA Counties/San Juan.gif';
+import Skagit from '../assets/WA Counties/Skagit.gif';
+import Skamania from '../assets/WA Counties/Skamania.gif';
+import Snohomish from '../assets/WA Counties/Snohomish.gif';
+import Spokane from '../assets/WA Counties/Spokane.gif';
+import Stevens from '../assets/WA Counties/Stevens.gif';
+import Thurston from '../assets/WA Counties/Thurston.gif';
+import Wahkiakum from '../assets/WA Counties/Wahkiakum.gif';
+import WallaWalla from '../assets/WA Counties/Walla Walla.gif';
+import Whatcom from '../assets/WA Counties/Whatcom.gif';
+import Whitman from '../assets/WA Counties/Whitman.gif';
+import Yakima from '../assets/WA Counties/Yakima.gif';
+
+export const counties = [
+    {
+        name: 'Adams',
+        img: Adams
+    },
+    {
+        name: 'Asotin',
+        img: Asotin
+    },
+    {
+        name: 'Benton',
+        img: Benton
+    },
+    {
+        name: 'Chelan',
+        img: Chelan
+    },
+    {
+        name: 'Clallam',
+        img: Clallam
+    },
+    {
+        name: 'Clark',
+        img: Clark
+    },
+    {
+        name: 'Columbia',
+        img: Columbia
+    },
+    {
+        name: 'Cowlitz',
+        img: Cowlitz
+    },
+    {
+        name: 'Douglas',
+        img: Douglas
+    },
+    {
+        name: 'Ferry',
+        img: Ferry
+    },
+    {
+        name: 'Franklin',
+        img: Franklin
+    },
+    {
+        name: 'Garfield',
+        img: Garfield
+    },
+    {
+        name: 'Grant',
+        img: Grant
+    },
+    {
+        name: 'Grays Harbor',
+        img: GraysHarbor
+    },
+    {
+        name: 'Island',
+        img: Island
+    },
+    {
+        name: 'Jefferson',
+        img: Jefferson
+    },
+    {
+        name: 'King',
+        img: King
+    },
+    {
+        name: 'Kitsap',
+        img: Kitsap
+    },
+    {
+        name: 'Kittitas',
+        img: Kittitas
+    },
+    {
+        name: 'Klickitat',
+        img: Klickitat
+    },
+    {
+        name: 'Lewis',
+        img: Lewis
+    },
+    {
+        name: 'Lincoln',
+        img: Lincoln
+    },
+    {
+        name: 'Mason',
+        img: Mason
+    },
+    {
+        name: 'Okanogan',
+        img: Okanogan
+    },
+    {
+        name: 'Pacific',
+        img: Pacific
+    },
+    {
+        name: 'Pend Oreille',
+        img: PendOreille
+    },
+    {
+        name: 'Pierce',
+        img: Pierce
+    },
+    {
+        name: 'San Juan',
+        img: SanJuan
+    },
+    {
+        name: 'Skagit',
+        img: Skagit
+    },
+    {
+        name: 'Skamania',
+        img: Skamania
+    },
+    {
+        name: 'Snohomish',
+        img: Snohomish
+    },
+    {
+        name: 'Spokane',
+        img: Spokane
+    },
+    {
+        name: 'Stevens',
+        img: Stevens
+    },
+    {
+        name: 'Thurston',
+        img: Thurston
+    },
+    {
+        name: 'Wahkiakum',
+        img: Wahkiakum
+    },
+    {
+        name: 'Walla Walla',
+        img: WallaWalla
+    },
+    {
+        name: 'Whatcom',
+        img: Whatcom
+    },
+    {
+        name: 'Whitman',
+        img: Whitman
+    },
+    {
+        name: 'Yakima',
+        img: Yakima
+    }
+]
---
+++ src/components/Options.jsx
@@ -0,0 +1,22 @@
+import { Button } from 'reactstrap';
+
+const Options = () => {
+    return (
+        <div>
+            <Button className='optionButton'>
+                Asotin
+            </Button>
+            <Button className='optionButton'>
+                Benton
+            </Button>
+            <Button className='optionButton'>
+                Snohomish
+            </Button>
+            <Button className='optionButton'>
+                King
+            </Button>
+        </div>
+    )
+}
+
+export default Options;
