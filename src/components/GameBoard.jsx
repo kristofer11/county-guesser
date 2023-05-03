a1d498d5033336506e52c616c7dd678cedd3f999
Kris Hvattum <klhvattum@live.com>
2023-05-02 22:47:19 -0700

    Rendering random map and random options

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,35 +1,70 @@
 import { useState } from 'react';
 import { counties } from '../assets/counties';
 import { Button } from 'reactstrap';
+import { useEffect } from 'react';
 
 const GameBoard = () => {
     const [currentCounty, setCurrentCounty] = useState(null);
+    const [buttonNames, setButtonNames] = useState([]);
     const [currentStreak, setCurrentStreak] = useState(0);
+    const [countyNameDisplay, setCountyNameDisplay] = useState('none');
 
-//Add 'handleClick' function for user selection
+    useEffect(() => {
+        const getRandomCounty = () => {
+            const randomIndex = Math.floor(Math.random() * counties.length);
+            const randomCountry = counties[randomIndex];
+            setCurrentCounty(randomCountry);
 
-//Add 'handleNextCounty' function for 'next' button
+            const names = [randomCountry.name]
+            while (names.length < 4) {
+                const randomName = counties[Math.floor(Math.random() * counties.length)].name;
+                if (!names.includes(randomName)) {
+                    names.push(randomName);
+                }
+            }
+            console.log(names)
+
+            const shuffledNames = names.sort(() => Math.random() - 0.5);
+            setButtonNames(shuffledNames);
+            console.log(buttonNames)
+        };
+
+        getRandomCounty();
+
+    }, []);
+
+
+    //Add 'handleClick' function for user selection
+
+    //Add 'handleNextCounty' function for 'next' button
 
     return (
-        <div>
-            <h5>Current Streak: {currentStreak}</h5>
-            <img src={counties[0].img} width='400px' />
-            <div className='buttonDiv'>
-                <Button className='optionButton'>
-                    Asotin
-                </Button>
-                <Button className='optionButton'>
-                    Benton
-                </Button>
-                <Button className='optionButton'>
-                    Snohomish
-                </Button>
-                <Button className='optionButton'>
-                    King
-                </Button>
-            </div>
+        <div className='gamePanel'>
+            {currentCounty ? (
+                <>
+                    <h5>Current Streak: {currentStreak}</h5>
+                    <img src={currentCounty.img} width='400px' />
+                    <div className='buttonDiv'>
+                        <Button className='optionButton'>
+                            {buttonNames[0]}
+                        </Button>
+                        <Button className='optionButton'>
+                            {buttonNames[1]}
+                        </Button>
+                        <Button className='optionButton'>
+                            {buttonNames[2]}
+                        </Button>
+                        <Button className='optionButton'>
+                            {buttonNames[3]}
+                        </Button>
+                    </div>
+                </>
+            ) : (
+                <p>loading</p>
+            )}
+
         </div>
     )
 }
 
-export default GameBoard
+export default GameBoard;
